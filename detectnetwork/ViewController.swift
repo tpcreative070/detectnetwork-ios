a6cb4da3ca08ccef9fdf9347589069d941d511a2
TPCreative <tpcreative.co@gmail.com>
2018-06-19 22:02:44 +0700

    Adding final code

--- detectnetwork/ViewController.swift
+++ detectnetwork/ViewController.swift
@@ -7,12 +7,66 @@
 //
 
 import UIKit
+import TPCreativeSwift
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        detectNetwork()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //declare this property where it won't go out of scope relative to your listener
+        let reachability = Reachability()!
+        //declare this inside of viewWillAppear
+        NotificationCenter.default.addObserver(self, selector: #selector(reachabilityChanged(note:)), name: .reachabilityChanged, object: reachability)
+        do{
+            try reachability.startNotifier()
+        }catch{
+            print("could not start reachability notifier")
+        }
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        /*
+         
+         reachability.stopNotifier()
+         NotificationCenter.default.removeObserver(self, name: .reachabilityChanged, object: reachability)
+         
+         */
+    }
+    
+    
+    @objc func reachabilityChanged(note: Notification) {
+        let reachability = note.object as! Reachability
+        switch reachability.connection {
+        case .wifi:
+            print("Reachable via WiFi")
+        case .cellular:
+            print("Reachable via Cellular")
+        case .none:
+            print("Network not reachable")
+        }
+    }
+    
+    
+    func detectNetwork(){
+        let reachabilityManager = NetworkReachabilityManager()
+        reachabilityManager?.startListening()
+        reachabilityManager?.listener = { _ in
+            if let isNetworkReachable = reachabilityManager?.isReachable,
+                isNetworkReachable == true {
+                //Internet Available
+                print("Internet available")
+            } else {
+                //Internet Not Available"
+                print("Internet not available")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +74,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
+
+
+
+
+
+
+/*
+ 
+ //declare this property where it won't go out of scope relative to your listener
+ let reachability = Reachability()!
+ 
+ reachability.whenReachable = { reachability in
+ if reachability.connection == .wifi {
+ print("Reachable via WiFi")
+ } else {
+ print("Reachable via Cellular")
+ }
+ }
+ reachability.whenUnreachable = { _ in
+ print("Not reachable")
+ }
+ 
+ do {
+ try reachability.startNotifier()
+ } catch {
+ print("Unable to start notifier")
+ }
+ 
+ 
+ 
+ //Stop nofitier
+ reachability.stopNotifier()
+ 
+ 
+ */
+
+
+
+
 
